af9c6e1a5faadb619594c30315884877ba2d4035
alexisdeslandes <alexis.deslandes@planisware.com>
2022-02-27 00:18:51 +0100

    Add layout package

--- lib/ad_template.dart
+++ lib/ad_template.dart
@@ -2,6 +2,7 @@ library ad_template;
 
 export 'src/app/app.dart';
 export 'src/common/common.dart';
+export 'src/layout/layout.dart';
 export 'src/providers/providers.dart';
 export 'src/router/router.dart';
 export 'src/theme/theme.dart';
---
+++ lib/src/layout/layout.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+part 'breakpoints.dart';
+
+part 'responsive_layout_builder.dart';
